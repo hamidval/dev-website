9611268edd9c8a32982f44d6b6957ceca59cd47c
hamidval <hamid-val@hotmail.co.uk>
2020-06-22 12:33:57 +0100

    Updates

---
+++ precache-manifest.ef7f8a8da405ce0ee186fcc894e4051a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89def338e8aed24d1da35621bcd41b6",
+    "url": "/dev-website/index.html"
+  },
+  {
+    "revision": "b7ab276bdc2d1f76c2e3",
+    "url": "/dev-website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b6bdf0b1d038142ed054",
+    "url": "/dev-website/static/css/main.e8a70e90.chunk.css"
+  },
+  {
+    "revision": "b7ab276bdc2d1f76c2e3",
+    "url": "/dev-website/static/js/2.c5f8e5cb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/dev-website/static/js/2.c5f8e5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6bdf0b1d038142ed054",
+    "url": "/dev-website/static/js/main.8dc4c58b.chunk.js"
+  },
+  {
+    "revision": "6443a8962a8a67ee77a6",
+    "url": "/dev-website/static/js/runtime-main.5df85480.js"
+  },
+  {
+    "revision": "6a4cf4853c511fba1f5b33b38e31f7cd",
+    "url": "/dev-website/static/media/hamid-back.6a4cf485.jpg"
+  },
+  {
+    "revision": "3ce27aeb3260b64dbb5d6a47c1b80207",
+    "url": "/dev-website/static/media/londonSky.3ce27aeb.jpg"
+  }
+]);
