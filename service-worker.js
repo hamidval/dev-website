e8855762408622d1c318783dd9736499045cd184
hamidval <hamid-val@hotmail.co.uk>
2020-07-28 17:05:09 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dev-website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
