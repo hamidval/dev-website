e9d534e12fbae32beeb9f1d6df41f5d989075121
hamidval <hamid-val@hotmail.co.uk>
2020-07-28 17:11:25 +0100

    changed file path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import PortfolioHamid from './components/PortfolioHamid'
+import ReactWhatsApp from './components/ReactWhatsApp'
 import './Custom.css';
 import './App.css';
 
@@ -24,6 +25,8 @@ class App extends Component {
       <Route exact path='/about' render={() => (<About  />)}/> 
       <Route exact path='/projects' render={() => (<Projects  />)}/> 
       <Route exact path='/hamid' render={() => (<PortfolioHamid  />)}/> 
+      <Route exact path='/whatsapp' render={() => (<ReactWhatsApp  />)}/> 
+
   </Switch>
  
     
