9f4b35b6abfd831a2104242c2c8b7b0013330dd0
hamidval <44161836+hamidval@users.noreply.github.com>
2020-11-01 20:03:52 +0000

    fixed nav bar and landing page

--- src/components/example_website/Home.js
+++ src/components/example_website/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter} from 'react-router-dom'
 import video from './video.mp4'
 import logo from './gailslogo.png'
-
+import {Navbar,Nav,NavDropdown} from 'react-bootstrap'
 
 
 
@@ -15,54 +15,41 @@ class Home extends Component {
         return ( 
             <div>
               <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
+              <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+              <Navbar.Brand href="#home">Gails</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#pricing">Pricing</Nav.Link>
+                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Nav>
+                  <Nav.Link href="#deets">More deets</Nav.Link>
+                  <Nav.Link eventKey={2} href="#memes">
+                    Dank memes
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
 
 
               </div>
 
-          <section class="wrapper">
-             <div class="video-wrap">
-              <div className="landing-text">
-                <img src={logo} className="gails-title"></img>
-              </div>
-           
-            <video loop src={video} muted={true} autoPlay>
-            <source   type="video/mp4"/>
+          <section className='video-container' >
+            <video loop className='video' src={video} autoPlay>
+
             </video>
-            </div>
-            <div class="overlay"></div>
+        
            
-          </section>
-          <section className="wrapper-2">
+          </section> 
+           <section className="wrapper-2">
            <div>
              info
            </div>
