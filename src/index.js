a6f76a2eb4e024710ababe17c3ff343a8b9b3530
hamidval <44161836+hamidval@users.noreply.github.com>
2022-08-25 18:09:43 +0100

    build

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
