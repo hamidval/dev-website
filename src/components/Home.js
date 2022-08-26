93d1be66a02c114e04099621f96945aedda8d163
hamidval <44161836+hamidval@users.noreply.github.com>
2022-08-26 12:13:26 +0100

    added tech skills section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React, { Component, createRef } from 'react';
 import { withRouter} from 'react-router-dom'
 import './home.css';
+import csharp from './images/C-Sharp.png'
+import react from './images/React-icon.png'
+import net from './images/net.png'
+import aws from './images/aws.png'
 
 
 
@@ -10,6 +14,11 @@ class Home extends Component {
     constructor(props){
         super(props);
         this.scrollDiv = createRef();
+        this.otherSkills = createRef();
+
+        this.state = {
+            chosenTechSkill: null
+        }
     }
 
     render() {
@@ -32,7 +41,7 @@ class Home extends Component {
                         <button class="nav-link btn btn-link" href="#technicalSkills" onClick={() => {this.scrollDiv.current.scrollIntoView({ behavior: 'smooth' }); }}>.TechnicalSkills( ) <span class="sr-only">(current)</span></button>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">.OtherSkills( )</a>
+                        <button class="nav-link btn btn-link" href="#otherSkills" onClick={() => {this.otherSkills.current.scrollIntoView({ behavior: 'smooth' }); }}>.OtherSkills( )</button>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">.Hobbies&Interests( )</a>
@@ -51,21 +60,34 @@ class Home extends Component {
                     <h2 id="technicalSkills" ref={this.scrollDiv} class="mt-3 mb-3">Technical Skills</h2>
                     <div class="card-container mt-3">
 
-                        <div class="card card-body skill-card">
-                                <h3>C#</h3>
+                        <div class={'card card-body skill-card '+(this.state.chosenTechSkill == 0? "chosen": null)} onClick={()=>{this.setState({chosenTechSkill: 0})}}>
+                             
+                                <img src={csharp}></img>
                         </div>
                         <div class="card card-body skill-card">
-                                <h3>React</h3>
+                        <img src={react}></img>
+                        </div>
+                        <div class={'card card-body skill-card '+(this.state.chosenTechSkill == 0? "chosen": null)}>
+                        <img src={net}></img>
                         </div>
                         <div class="card card-body skill-card">
-                                <h3>.NET</h3>
-                        </div>
-                        <div class="card card-body skill-card">
-                                <h3>AWS</h3>
+                        <img src={aws}></img>
                         </div>
 
                     </div>
 
+
+                    <div id="skillText" class="mt-3 card card-body h4">
+                        {
+                            this.state.chosenTechSkill == 0? "Junior Developer at Reed.co.uk, using the following technologies C# .NET MVC, VueJs, MicrosoftSqlServer, Azure Devops, to build and maintain the Reed Courses Ecommerce platform": null
+                        }
+                    </div>
+
+                </div>
+
+
+                <div id="page3" class="page">
+                        <h2 id="otherSkills" ref={this.otherSkills}>Other skills</h2>
                 </div>
   
             </div>
